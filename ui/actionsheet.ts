115b1a50481f3226fb8395f13e3e53b9d333d43f
Robert Arnesson <robert@robertarnesson.com>
2016-10-24 14:53:17 -0400

    loading and actionsheet fixes

--- ui/actionsheet.ts
+++ ui/actionsheet.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'actionsheet',
@@ -11,20 +11,6 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 export class ActionsheetComponent {
     constructor(private elementRef: ElementRef) {
         actionsheetComponent = this;
-    }
-
-    ngOnInit() {
-        (<any>Object).assign(this.elementRef.nativeElement.style, {
-            visibility: 'hidden',
-            opacity: '0',
-            position: 'absolute',
-            top: '0',
-            bottom: '0',
-            left: '0',
-            right: '0',
-            zIndex: '100',
-            background: 'rgba(0, 0, 0, 0.6)'
-        });
     }
 
     public actions: any[] = [];
--- ui/loading.ts
+++ ui/loading.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'loading',
@@ -9,20 +9,6 @@ import { Component, ElementRef, OnInit } from '@angular/core';
 export class LoadingComponent {
     constructor(private elementRef: ElementRef) {
         loadingComponent = this;
-    }
-
-    ngOnInit() {
-        (<any>Object).assign(this.elementRef.nativeElement.style, {
-            visibility: 'hidden',
-            opacity: '0',
-            position: 'absolute',
-            top: '0',
-            bottom: '0',
-            left: '0',
-            right: '0',
-            zIndex: '100',
-            background: 'rgba(0, 0, 0, 0.6)'
-        });
     }
 
     public show(): void {
