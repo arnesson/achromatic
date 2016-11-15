830cc78cc870d132dc0fb4708ba7334d2bff08a0
Robert Arnesson <robert@robertarnesson.com>
2016-11-15 14:54:32 -0500

    lazyload work

--- lazyload.ts
+++ lazyload.ts
@@ -7,13 +7,15 @@ export class LazyloadDirective implements OnInit {
 	constructor(private elementRef: ElementRef) {}
 
 	@Input('lazyload') file: string;
-	@Input('preview') preview: string;
+	@Input('placeholder') placeholder: string;
 	@Input('width') width: number | string;
 	@Input('height') height: number | string;
 
 	ngOnInit() {
 		let width = this.width || 'auto';
 		let height = this.height || 'auto';
+		// TODO: maybe have a nicer placeholder that looks cool with the blur filter
+		let placeholder = this.placeholder || 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
 
 		(<any>Object).assign(this.elementRef.nativeElement.style, {
 			width: typeof width === 'number' ? `${width}px` : width,
@@ -21,16 +23,18 @@ export class LazyloadDirective implements OnInit {
 			display: 'block',
 			backgroundColor: 'rgba(0,0,0,.1)',
 			backgroundSize: 'cover',
-			backgroundImage: `url(${this.url()})`
+			backgroundImage: `url(${this.base64(placeholder)})`,
+			webkitFilter: 'blur(15px)',
+			mozFilter: 'blur(15px)',
+			oFilter: 'blur(15px)',
+			msFilter: 'blur(15px)',
+			filter: 'blur(15px)'
 		});
 	}
 
-	private url(): string {
+	private base64(url: string): string {
 		let base64 = /^(?:[A-Z0-9+\/]{4})*(?:[A-Z0-9+\/]{2}==|[A-Z0-9+\/]{3}=|[A-Z0-9+\/]{4})$/i;
 
-		if (!this.file) {
-			return 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7';
-		}
 		if (base64.test(this.file)) {
 			return 'data:application/octet-stream;base64,' + this.file;
 		} else {
