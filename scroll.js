41efa6d0d8fde33fc4c858cea6060e9749650594
Robert Arnesson <robert@robertarnesson.com>
2017-02-09 14:22:33 -0500

    use native scroll

--- scroll.js
+++ scroll.js
@@ -9,87 +9,18 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var iscroll_1 = require('./iscroll');
 var ScrollDirective = (function () {
     function ScrollDirective(elementRef) {
         this.elementRef = elementRef;
-        this.iscroll = new iscroll_1.IScroll(this.elementRef.nativeElement, this.elementRef.nativeElement.parentNode, {
-            scrollX: false,
-            scrollY: true,
-            freeScroll: false,
-            eventPassthrough: 'horizontal',
-            momentum: true,
-            scrollbars: true,
-            mouseWheel: true,
-            fadeScrollbars: true
+        Object.assign(this.elementRef.nativeElement.style, {
+            overflowY: 'auto',
+            overflowX: 'hidden',
+            webkitOverflowScrolling: 'touch',
+            willChange: 'scroll-position',
+            contain: 'size style layout'
         });
     }
-    ScrollDirective.prototype.x = function () {
-        return this.iscroll && this.iscroll.x;
-    };
-    ScrollDirective.prototype.y = function () {
-        return this.iscroll && this.iscroll.y;
-    };
-    ScrollDirective.prototype.maxScrollX = function () {
-        return this.iscroll && this.iscroll.maxScrollX;
-    };
-    ScrollDirective.prototype.maxScrollY = function () {
-        return this.iscroll && this.iscroll.maxScrollY;
-    };
-    ScrollDirective.prototype.refresh = function () {
-        var _this = this;
-        if (this.iscroll) {
-            window.setTimeout(function () {
-                if (_this.iscroll) {
-                    _this.iscroll.refresh();
-                }
-            }, 1);
-        }
-    };
-    ScrollDirective.prototype.scrollTo = function (x, y, time) {
-        var _this = this;
-        if (this.iscroll) {
-            window.setTimeout(function () {
-                if (_this.iscroll) {
-                    _this.iscroll.scrollTo(x, y, time);
-                }
-            }, 1);
-        }
-    };
-    ScrollDirective.prototype.scrollToTop = function (time) {
-        var _this = this;
-        if (this.iscroll) {
-            window.setTimeout(function () {
-                if (_this.iscroll) {
-                    _this.iscroll.scrollTo(0, 0, time);
-                }
-            }, 1);
-        }
-    };
-    ScrollDirective.prototype.scrollToBottom = function (time) {
-        var _this = this;
-        if (this.iscroll) {
-            window.setTimeout(function () {
-                if (_this.iscroll) {
-                    _this.iscroll.scrollTo(0, _this.iscroll.maxScrollY, time);
-                }
-            }, 1);
-        }
-    };
-    ScrollDirective.prototype.on = function (event, fn) {
-        if (this.iscroll) {
-            if (this.iscroll) {
-                this.iscroll.on(event, function () {
-                    fn(this);
-                });
-            }
-        }
-    };
     ScrollDirective.prototype.ngOnDestroy = function () {
-        if (this.iscroll) {
-            this.iscroll.destroy();
-        }
-        this.iscroll = null;
     };
     ScrollDirective = __decorate([
         core_1.Directive({
--- scroll.ts
+++ scroll.ts
@@ -1,93 +1,20 @@
 import { Directive, ElementRef, OnDestroy } from '@angular/core';
 
-import { IScroll } from './iscroll';
-
 @Directive({
   selector: '[scroll]'
 })
 export class ScrollDirective implements OnDestroy {
-  private iscroll: IScroll;
-
   constructor(private elementRef: ElementRef) {
-    this.iscroll = new IScroll(this.elementRef.nativeElement, this.elementRef.nativeElement.parentNode, {
-      scrollX: false,
-      scrollY: true,
-      freeScroll: false,
-      eventPassthrough: 'horizontal',
-      momentum: true,
-      scrollbars: true,
-      mouseWheel: true,
-      fadeScrollbars: true
+    (<any>Object).assign(this.elementRef.nativeElement.style, {
+        overflowY: 'auto',
+        overflowX: 'hidden',
+        webkitOverflowScrolling: 'touch',
+        willChange: 'scroll-position',
+        contain: 'size style layout'
     });
   }
 
-  public x(): number {
-    return this.iscroll && this.iscroll.x;
-  }
-  public y(): number {
-    return this.iscroll && this.iscroll.y;
-  }
-  public maxScrollX(): number {
-    return this.iscroll && this.iscroll.maxScrollX;
-  }
-  public maxScrollY(): number {
-    return this.iscroll && this.iscroll.maxScrollY;
-  }
-
-  public refresh() {
-    if (this.iscroll) {
-      (<any>window).setTimeout(() => {
-        if (this.iscroll) {
-          this.iscroll.refresh();
-        }
-      }, 1);
-    }
-  }
-
-  public scrollTo(x: number, y: number, time?: number) {
-    if (this.iscroll) {
-      (<any>window).setTimeout(() => {
-        if (this.iscroll) {
-          this.iscroll.scrollTo(x, y, time);
-        }
-      }, 1);
-    }
-  }
-
-  public scrollToTop(time?: number) {
-    if (this.iscroll) {
-      (<any>window).setTimeout(() => {
-        if (this.iscroll) {
-          this.iscroll.scrollTo(0, 0, time);
-        }
-      }, 1);
-    }
-  }
-
-  public scrollToBottom(time?: number) {
-    if (this.iscroll) {
-      (<any>window).setTimeout(() => {
-        if (this.iscroll) {
-          this.iscroll.scrollTo(0, this.iscroll.maxScrollY, time);
-        }
-      }, 1);
-    }
-  }
-
-  public on(event: string, fn: (scroller: IScroll) => void) {
-    if (this.iscroll) {
-      if (this.iscroll) {
-        this.iscroll.on(event, function() {
-          fn(this);
-        });
-      }
-    }
-  }
-
   ngOnDestroy() {
-    if (this.iscroll) {
-      this.iscroll.destroy();
-    }
-    this.iscroll = null;
+    
   }
 }
